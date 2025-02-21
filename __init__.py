eb1b39a994a7c1dca4fad26772248d1239505202
jordan wakelam <jwakelams@gmail.com>
2025-02-21 10:55:08 +0100

    création de la route decrypt et tranform return en json pour manip avec postman

--- __init__.py
+++ __init__.py
@@ -1,24 +1,35 @@
 from cryptography.fernet import Fernet
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
-from flask import json
+from flask import jsonify
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
+
 key = Fernet.generate_key()
 f = Fernet(key)
+
 
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+    # return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    return jsonify({"token_encrypt": token}), 200  # Retourne le token en str
+
+
+@app.route('/decrypt/<string:valeur>')
+def decryptage(valeur):
+    token = valeur
+    return jsonify({"token_decrypt": token}), 200
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
