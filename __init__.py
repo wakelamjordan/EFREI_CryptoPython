9abfa03593aebdcabe60b8c72dbc199c80225f15
jordan wakelam <jwakelams@gmail.com>
2025-02-21 14:57:40 +0100

    ajout de quelques commentaires dans _make_key()

--- __init__.py
+++ __init__.py
@@ -19,18 +19,23 @@ def hello_world():
 
 
 def _make_key(key: str) -> Fernet:
+    # key doit être adapté pour satisfaire aux caractéristique des paramètres qu'accepte Fernet
+    # URL-safe base64-encoded 32-byte key
+    # ajustement pour avoir 32 charactéres
     key = key.ljust(32)[:32]
+    # conversion en bytes
     key_byte = key.encode()
+    # base64 - encoded et URL-safe
     key_base64 = base64.urlsafe_b64encode(key_byte)
-
+    print(key_base64)
+    # instanciation de l'objet avec la clé compatible avec ses algorythmes Fernet
     f = Fernet(key_base64)
-
     return f
 
 
 @app.route('/encrypt/<string:key>/<string:valeur>')
 def encryptage(key, valeur):
-
+    # récupération de la clée dans l'url mise en paramètre dans _make_key
     f = _make_key(key)
 
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
