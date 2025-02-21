0deb7d977b65c78c29198e89fb1ab1fede336d14
jordan wakelam <jwakelams@gmail.com>
2025-02-21 15:19:09 +0100

    rectification de _make_key

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask import render_template
 from flask import jsonify
 from urllib.request import urlopen
 import sqlite3
-import base64
+import base64, hashlib
 
 app = Flask(__name__)
 
@@ -19,16 +19,21 @@ def hello_world():
 
 
 def _make_key(key: str) -> Fernet:
-    # key doit être adapté pour satisfaire aux caractéristique des paramètres qu'accepte Fernet
-    # URL-safe base64-encoded 32-byte key
-    # ajustement pour avoir 32 charactéres
-    key = key.ljust(32)[:32]
-    # conversion en bytes
-    key_byte = key.encode()
-    # base64 - encoded et URL-safe
-    key_base64 = base64.urlsafe_b64encode(key_byte)
-    print(key_base64)
-    # instanciation de l'objet avec la clé compatible avec ses algorythmes Fernet
+    # # key doit être adapté pour satisfaire aux caractéristique des paramètres qu'accepte Fernet
+    # # URL-safe base64-encoded 32-byte key
+    # # ajustement pour avoir 32 charactéres
+    # key = key.ljust(32)[:32]
+    # # conversion en bytes
+    # key_byte = key.encode()
+    # # base64 - encoded et URL-safe
+    # key_base64 = base64.urlsafe_b64encode(key_byte)
+    # # print(key_base64)
+    # # instanciation de l'objet avec la clé compatible avec ses algorythmes Fernet
+
+    # ------------------------------
+    key_hash = hashlib.sha256(key.encode())
+    key_32 = key_hash.digest()
+    key_base64 = base64.urlsafe_b64encode(key_32)
     f = Fernet(key_base64)
     return f
 
